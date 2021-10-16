7a2ee80e9936ce9af6afe85ec328ca064d6edfb1
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 01:09:22 +0300

    Add files via upload

---
+++ module_01/Seminar_12/Classwork/Task_04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_04
+{
+    class Program
+    {
+
+        public static int NumOfDigits(Match x)
+        {
+            string strX = x.Value.ToString();
+            return strX.Length;
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Regex reg = new Regex(@"[0-9]*");
+            var mat = reg.Matches(s);
+            int maxNum = 0;
+            foreach(Match x in mat)
+            {
+                if (NumOfDigits(x) > maxNum)
+                {
+                    maxNum = NumOfDigits(x);
+                }
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
