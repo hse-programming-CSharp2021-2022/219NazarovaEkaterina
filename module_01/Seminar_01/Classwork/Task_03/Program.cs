e87f04953b7d58d006e061a13000f24c6104c05b
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-16 23:34:54 +0300

    Add files via upload

---
+++ module_01/Seminar_01/Classwork/Task_03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine("Привет, " + a);
+        }
+    }
+}
