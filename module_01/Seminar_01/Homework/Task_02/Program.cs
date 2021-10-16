46db6531831c8df221cb5d02b7845ad583e1ec65
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-16 23:40:40 +0300

    Add files via upload

---
+++ module_01/Seminar_01/Homework/Task_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task0202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("##   ##    #### ######    #### ######   ####  ##     ##    ####");
+            Console.WriteLine("###  ##   ## ## ######   ## ## ##  ##  ##  ## ##     ##   ## ##");
+            Console.WriteLine("#### ##  ##  ##    ###  ##  ## ##   ## ##  ##  ##   ##   ##  ##");
+            Console.WriteLine("####### ##   ##  ####  ##   ## ##  ##  ##  ##  ##   ##  ##   ##");
+            Console.WriteLine("## #### ####### ###    ####### #####   ##  ##   ## ##   #######");
+            Console.WriteLine("##  ### ##   ## ###### ##   ## ## ###  ##  ##   ## ##   ##   ##");
+            Console.WriteLine("##   ## ##   ## ###### ##   ## ##  ###  ####     ###    ##   ##");
+        }
+    }
+}
