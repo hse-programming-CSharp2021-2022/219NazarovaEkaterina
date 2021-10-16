107e5c2547c4c2a3fbd28bd4bb27af06207bf9ae
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:37:09 +0300

    Add files via upload

---
+++ module_01/Seminar_04/Classwork/Task_05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        public static int NumDel(int a)
+        {
+            int col = 0;
+            for(int i = 1; i < ((int)Math.Sqrt(a)); i++)
+            {
+                if(a % i == 0)
+                {
+                    col = col + 2;
+                }
+            }
+            if (Math.Sqrt(a) == (int)Math.Sqrt(a))
+            {
+                col = col + 1;
+            }
+            return col;
+        }
+        static void Main(string[] args)
+        {
+            int k;
+            string s = Console.ReadLine();
+            int.TryParse(s, out k);
+            for(int i = 100; i<1000; i++)
+            {
+                if(NumDel(i) == k)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
