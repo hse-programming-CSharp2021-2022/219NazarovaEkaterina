b263c44aa0e4b287f3a7ef5283c6f10d8911f674
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:40:24 +0300

    Add files via upload

---
+++ module_01/Seminar_05/Classwork/Task_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static void Sums(uint number, out uint sumEven, out uint sumOdd)
+        {
+            uint Even = 0, Odd = 0, prom;
+            string s = number.ToString();
+            for(int i = 1; i <= s.Length; i++)
+            {
+                prom = number % 10;
+                if ((i % 2) == 1)
+                {
+                    Odd += prom;
+                }
+                else
+                {
+                    Even += prom;
+                }
+                number = number / 10;
+            }
+            sumEven = Even;
+            sumOdd = Odd;
+            
+
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            uint.TryParse(s, out uint i);
+            uint a, b;
+            Sums(i, out a, out b);
+            Console.WriteLine("SumEven = {0}, SumOdd = {1}", a, b);
+        }
+    }
+}
