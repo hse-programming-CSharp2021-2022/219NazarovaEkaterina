b067c954b5a2446ae11f076b0706a1af4c398b10
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:58:29 +0300

    Add files via upload

---
+++ module_01/Seminar_08/Homework/Task_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SnakeArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк/столбцов в массиве");
+            int.TryParse(Console.ReadLine(), out int n);
+            var mas = new int[n, n];
+            int ringCounter = 0, numberCounter = 1;
+            while (numberCounter <= n * n)
+            {
+                for(var i = ringCounter; i < n - ringCounter; i++)
+                {
+                    mas[ringCounter, i] = numberCounter++;
+                }
+                numberCounter--;
+                for(var j = ringCounter; j < n - ringCounter; j++)
+                {
+                    mas[j, n - ringCounter - 1] = numberCounter++;
+                }
+                numberCounter--;
+                for(var i = n - ringCounter - 1; i >= ringCounter; i--)
+                {
+                    mas[n - ringCounter - 1, i] = numberCounter++;
+                }
+                numberCounter--;
+                for(var j = n - ringCounter - 1; j > ringCounter; j--)
+                {
+                    mas[j, ringCounter] = numberCounter++;
+                }
+                ringCounter++;
+            }
+            for(var i = 0; i<n; i++)
+            {
+                for(var j = 0; j<n; j++)
+                {
+                    Console.Write("{0, 3} ", mas[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
