e152bf33350652c7d1b52ae7643e3ee0d7af793e
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:14:42 +0300

    Add files via upload

---
+++ module_01/Seminar_02/Homework/Task_06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        public static void Budget(string x, string y)
+        {
+            double sum, a, b;
+            double.TryParse(x, out a);
+            double.TryParse(y, out b);
+            b = b / 100;
+            sum = a * b;
+            decimal sum2 = (decimal)sum;
+            Console.WriteLine($"{sum2:C}");
+        }
+        static void Main(string[] args)
+        {
+            string s1, s2;
+            do
+            {
+                Console.WriteLine("Введите бюджет (вещественное число): ");
+                s1 = Console.ReadLine();
+                Console.WriteLine("Введите кол-во процентов, приходящееся на компьютерные игры: ");
+                s2 = Console.ReadLine();
+                Budget(s1, s2);
+                Console.WriteLine("Для выхода нажмите Esc; для продолжения нажмите Enter");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
