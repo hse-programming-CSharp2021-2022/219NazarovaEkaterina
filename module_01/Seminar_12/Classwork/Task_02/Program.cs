3d0fc1db7823ad38f2c0cd478931561c3a563ddd
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 01:07:12 +0300

    Add files via upload

---
+++ module_01/Seminar_12/Classwork/Task_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Regex reg = new Regex(@"[^a-z]*[a-z][a-z][a-z][a-z][^a-z]");  // или \b\w{4}\b
+            var matches = reg.Matches(s);
+            foreach (Match x in matches)
+                Console.WriteLine(x);
+        }
+    }
+}
