8cad2714bf0892afbe36b7522997d3d645432499
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:52:34 +0300

    Add files via upload

---
+++ module_01/Seminar_07/Classwork/Task_02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        public static void PrintMas(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write("{0} ", mas[i]);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+            for(int i = 0; i <100; i++)
+            {
+                array[i] = i+1;
+            }
+            Random rand = new Random();
+            int prom, prom2, c;
+            for(int i = 0; i < 200; i++)
+            {
+                prom = rand.Next(0, 100);
+                prom2 = rand.Next(0, 100);
+                c = array[prom];
+                array[prom] = array[prom2];
+                array[prom2] = c;
+            }
+            Array.Resize(ref array, 99);
+            int sum1 = 0;
+            for(int i = 0; i < 99; i++)
+            {
+                sum1 += array[i];
+            }
+            Console.WriteLine(5050 - sum1);
+            
+        }
+    }
+}
