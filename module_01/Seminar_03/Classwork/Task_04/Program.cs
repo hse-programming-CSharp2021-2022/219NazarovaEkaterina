31c297d34e7c7cbd5582d2343fe527facd636464
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:26:45 +0300

    Add files via upload

---
+++ module_01/Seminar_03/Classwork/Task_04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_04
+{
+    class Program
+    {
+        public static void Sw(int a)
+        {
+            int p, m = 0;
+            while (a > 0)
+            {
+                m *= 10;
+                p = a % 10;
+                a = a / 10;
+                m += p;
+            }
+            Console.WriteLine(m);
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int i;
+            int.TryParse(s, out i);
+            Sw(i);
+        }
+    }
+}
