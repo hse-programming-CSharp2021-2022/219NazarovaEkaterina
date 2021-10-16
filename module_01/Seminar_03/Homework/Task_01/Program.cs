a9e1ee83530ed201b2b1d6b080a283c3b6b14d66
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:28:44 +0300

    Add files via upload

---
+++ module_01/Seminar_03/Homework/Task_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static bool F(string x, string y)
+        {
+            double x1, y1, two = Math.Sqrt(2);
+            double.TryParse(x, out x1);
+            double.TryParse(y, out y1);
+            if ((x1 >= 0) && (x1 <= two) && (y1 >= -2) && (y1 <= two))
+            {
+                return true;
+            }
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+            string s1, s2;
+            do
+            {
+                Console.WriteLine("Введите координаты точки");
+                s1 = Console.ReadLine();
+                s2 = Console.ReadLine();
+                Console.WriteLine("Результат: {0}", F(s1, s2));
+                Console.WriteLine("Для выхода нажмите Esc; для продолжения нажмите Enter");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
