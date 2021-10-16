d2efc53f867006e90965adb6995f4bd9081938ea
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:29:36 +0300

    Add files via upload

---
+++ module_01/Seminar_03/Homework/Task_02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static double G(string x, string y)
+        {
+            double x1, y1;
+            double.TryParse(x, out x1);
+            double.TryParse(y, out y1);
+            if ((x1 < y1) && (x1 > 0))
+            {
+                return x1 + Math.Sin(y1);
+            }
+            else if ((x1 > y1) && (x1 < 0))
+            {
+                return y1 - Math.Cos(x1);
+            }
+            else
+            {
+                return 0.5 * x1 * y1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string s1, s2;
+            do
+            {
+                Console.WriteLine("Введите значения X и Y: ");
+                s1 = Console.ReadLine();
+                s2 = Console.ReadLine();
+                Console.WriteLine("G = {0}", G(s1, s2));
+                Console.WriteLine("Для выхода нажмите Esc; для продолжения нажмите Enter");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
