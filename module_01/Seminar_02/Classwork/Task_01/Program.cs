0e9fbc17f6db7356a6ef255b7e5b8783d0e6f917
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:04:21 +0300

    Add files via upload

---
+++ module_01/Seminar_02/Classwork/Task_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static void Cut(string s)
+        {
+            int l = s.Length;
+            int a;
+            int.TryParse(s, out a);
+            int i, dec = 1;
+            for (i = 1; i < l; i++)
+            {
+                dec = dec * 10;
+            }
+            while(a>0)
+            {
+                Console.WriteLine(a / dec);
+                a = a % dec;
+                dec = dec / 10;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string ss = Console.ReadLine();
+            Cut(ss);
+        }
+    }
+}
