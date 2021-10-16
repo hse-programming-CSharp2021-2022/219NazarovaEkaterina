62f4895aa1989f1eb8a35dcfaf18035a7533239a
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-16 23:36:03 +0300

    Add files via upload

---
+++ module_01/Seminar_01/Classwork/Task_04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "5 / 3 = " + 5 / 3;
+            Console.WriteLine(result);
+            result = "5.0 / 3.0 = " + 5.0 / 3.0;
+            Console.WriteLine(result);
+            result = " " + 5 / 3;
+            Console.WriteLine(result);
+        }
+    }
+}
