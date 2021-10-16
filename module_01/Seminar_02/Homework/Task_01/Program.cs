d9294ea6bfcf810584afb00b4231f6311de4972b
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:09:22 +0300

    Add files via upload

---
+++ module_01/Seminar_02/Homework/Task_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static int Polynom(int x)
+        {
+            int b = x * x;
+            return (b * (12 * b + 9 * x - 3) + 2 * x - 4);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x: ");
+            string s;
+            do
+            {
+                s = Console.ReadLine();
+                if (s != "End")
+                {
+                    int i, res;
+                    int.TryParse(s, out i);
+                    res = Polynom(i);
+                    Console.WriteLine("Результат: " + res);
+                    Console.WriteLine("Для выхода введите End; чтобы продолжить, введите х");
+                }
+            } while (s != "End");
+        }
+    }
+}
