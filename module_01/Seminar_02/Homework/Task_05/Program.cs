9ad8a8369b0163b8339302c1ef4236ad2c346234
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:13:47 +0300

    Add files via upload

---
+++ module_01/Seminar_02/Homework/Task_05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        public static void NotEqual(string x, string y, string z)
+        {
+            double a, b, c;
+            double.TryParse(x, out a);
+            double.TryParse(y, out b);
+            double.TryParse(z, out c);
+            string d;
+            d = ((a < b + c) && (b < a + c) && (c < a + b)) ? "Неравенство треугольника выполняется" : "Неравенство треугольника не выполняется";
+            Console.WriteLine(d);
+        }
+        static void Main(string[] args)
+        {
+            string s1, s2, s3;
+            do
+            {
+                Console.WriteLine("Введите 3 вещественных числа: ");
+                s1 = Console.ReadLine();
+                s2 = Console.ReadLine();
+                s3 = Console.ReadLine();
+                NotEqual(s1, s2, s3);
+                Console.WriteLine("Для выхода нажмите Esc; для продолжения нажмите Enter");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
