a802d9d0f2efc5ecd3eac0aa4c8fa5a06b189e53
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:33:49 +0300

    Add files via upload

---
+++ module_01/Seminar_04/Classwork/Task_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2, c = 3;
+            double sum2 = (1 / ((double)(a + b + c))), sum1 = 0;
+            while(sum1 < sum2)
+            {
+                sum1 = sum2;
+                a++; b++; c++;
+                sum2 = sum2 + (1 / ((double)(a + b + c)));
+            }
+            Console.WriteLine(sum2);
+        }
+    }
+}
