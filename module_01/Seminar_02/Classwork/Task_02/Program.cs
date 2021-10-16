b7f498b74bd48d111b235517c942f8a0fb16be8a
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:05:30 +0300

    Add files via upload

---
+++ module_01/Seminar_02/Classwork/Task_02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Radius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            double r;
+            double.TryParse(s, out r);
+            double l, sq;
+            l = 2 * 3.14159265 * r;
+            sq = 3.14159265 * r * r;
+            double res1 = l * 1000, res2 = sq * 1000;
+            int i = (int)res1, i2 = (int)res2;
+            res1 = i;
+            res2 = i2;
+            Console.WriteLine("Длина = " + res1/1000);
+            Console.WriteLine("Площадь = " + "{0:0.000}", res2/1000);
+        }
+    }
+}
