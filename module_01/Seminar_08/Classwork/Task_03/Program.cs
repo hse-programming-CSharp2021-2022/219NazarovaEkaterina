364b52a2de0f02298465e670c17439e24ab4d19b
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:56:57 +0300

    Add files via upload

---
+++ module_01/Seminar_08/Classwork/Task_03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int n);
+            Random rand = new Random();
+            int[][] mas = new int[n][];
+            for(int i = 0; i<n; i++)
+            {
+                mas[i] = new int[rand.Next(5, 16)];
+                for(int j = 0; j < mas[i].Length; j++)
+                {
+                    mas[i][j] = rand.Next(-10, 11);
+                    Console.Write("{0} ", mas[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for(int i = 0; i < n; i++)
+            {
+                Array.Sort(mas[i], (int a, int b) =>
+                {
+                    if (a > b) return -1;
+                    if (a < b) return 1;
+                    else return 0;
+                });
+            }
+            Array.Sort(mas, (int[] a, int[] b) =>
+            {
+                if (a.Length > b.Length) return -1;
+                if (a.Length < b.Length) return 1;
+                else return 0;
+            });
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < mas[i].Length; j++)
+                {
+                    Console.Write("{0} ", mas[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
