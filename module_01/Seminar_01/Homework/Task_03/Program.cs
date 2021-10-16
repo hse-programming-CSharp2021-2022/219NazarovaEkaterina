76feae36e8d8d69dcfae0cd1677023c0aa47bc8c
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-16 23:41:43 +0300

    Add files via upload

---
+++ module_01/Seminar_01/Homework/Task_03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ASCIIDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Code;
+            string input = Console.ReadLine();
+            int.TryParse(input, out Code);
+            Console.WriteLine((char)Code);
+        }
+    }
+}
