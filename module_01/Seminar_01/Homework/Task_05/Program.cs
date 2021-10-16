2f45b1bfe0401b9dd95d2b4705c306a4f8e453bb
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-16 23:45:01 +0300

    Add files via upload

---
+++ module_01/Seminar_01/Homework/Task_05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k1, k2;
+            string inp1 = Console.ReadLine();
+            int.TryParse(inp1, out k1);
+            string inp2 = Console.ReadLine();
+            int.TryParse(inp2, out k2);
+            Console.WriteLine(Math.Sqrt((k1 * k1) + (k2 * k2)));
+        }
+    }
+}
