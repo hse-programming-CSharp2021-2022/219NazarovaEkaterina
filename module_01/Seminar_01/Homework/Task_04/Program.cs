1232f5422b63c6740e538d543857b5a9ad32860d
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-16 23:43:59 +0300

    Add files via upload

---
+++ module_01/Seminar_01/Homework/Task_04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double U, R;
+            string inp1 = Console.ReadLine();
+            double.TryParse(inp1, out U);
+            string inp2 = Console.ReadLine();
+            double.TryParse(inp2, out R);
+            Console.WriteLine("I = " + U / R);
+            Console.WriteLine("P = " + (U * U) / R);
+        }
+    }
+}
