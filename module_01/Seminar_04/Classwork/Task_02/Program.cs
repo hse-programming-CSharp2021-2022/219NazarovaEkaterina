a09d5faf5793ee8d4c0ac84d14517bc69d381ffd
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:34:48 +0300

    Add files via upload

---
+++ module_01/Seminar_04/Classwork/Task_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, delta;
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string s3 = Console.ReadLine();
+            double.TryParse(s1, out a);
+            double.TryParse(s2, out b);
+            double.TryParse(s3, out delta);
+            double x1 = a, x2 = a + delta;
+            double s = 0;
+            do
+            {
+                s = s + (((x1 * x1 + x2 * x2) / 2) * delta);
+                x1 = x2;
+                x2 = x2 + delta;
+            } while ((b - delta) < delta);
+            x2 = b - x1;
+            s = s + (((x1 * x1 + x2 * x2) / 2) * delta);
+            Console.WriteLine(s);
+
+
+        }
+    }
+}
