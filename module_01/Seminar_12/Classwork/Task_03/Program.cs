0c8c793b1f60bed14b1f14621568df9766eaf508
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 01:08:38 +0300

    Add files via upload

---
+++ module_01/Seminar_12/Classwork/Task_03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); 
+            Regex regOne = new Regex(@"\b[a-zA-Z]*a\b");
+            var mat = regOne.Matches(s);
+            foreach (var x in mat)
+                Console.Write("{0} ", x);
+        }
+    }
+}
