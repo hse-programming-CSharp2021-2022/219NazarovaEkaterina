01029ed4dc93db33808fee78d2916e39502b4b5d
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:24:57 +0300

    Add files via upload

---
+++ module_01/Seminar_03/Classwork/Task_02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        public static void Ffor()
+        {
+            int k;
+            for(int i = 2; i<=20; i+=2)
+            {
+                Console.WriteLine(i * i);
+            }
+        }
+        public static void Fwhile()
+        {
+            int i = 2;
+            while (i <= 20)
+            {
+                Console.WriteLine(i * i);
+                i += 2;
+            }
+        }
+        public static void FDoWhile()
+        {
+            int i = 2;
+            do
+            {
+                Console.WriteLine(i * i);
+                i += 2;
+            }
+            while (i <= 20);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For: ");
+            Ffor();
+            Console.WriteLine("While: ");
+            Fwhile();
+            Console.WriteLine("Do-While: ");
+            FDoWhile();
+        }
+    }
+}
