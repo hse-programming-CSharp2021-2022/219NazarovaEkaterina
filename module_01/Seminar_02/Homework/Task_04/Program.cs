b82dddb6817c0f3ab570c29054ac5aedb8251c84
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:12:34 +0300

    Add files via upload

---
+++ module_01/Seminar_02/Homework/Task_04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        public static void Swi(string x)
+        {
+            int i, m=0;
+            int.TryParse(x, out i);
+            while (i > 0)
+            {
+                m *= 10;
+                m += (i % 10);
+                i = i / 10;
+            }
+            Console.WriteLine(m);
+        }
+        static void Main(string[] args)
+        {
+            string s;
+            do
+            {
+                Console.WriteLine("Введите четырёхзначное число: ");
+                s = Console.ReadLine();
+                Swi(s);
+                Console.WriteLine("Для выхода нажмите Esc; для продолжения нажмите Enter");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
