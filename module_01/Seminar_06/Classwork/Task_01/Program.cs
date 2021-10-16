1d5d9bc2fa525335a1389fe3a61237a01a9d7c1d
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:42:25 +0300

    Add files via upload

---
+++ module_01/Seminar_06/Classwork/Task_01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 0, i = 0, nomer, prom, a;
+            
+            string s = Console.ReadLine();
+            int n = s.Length;
+            int.TryParse(s, out int num);
+
+            int max = 0;
+
+            int j = 1;
+            while (num > 0)
+            {
+                j++;
+                prom = num % 10;
+                if (prom > max)
+                {
+                    max = prom;
+                    nomer = j;
+                }
+                num = num / 10;
+            }
+            m *= 10;
+            m += max;
+            num = num - max * (int)Math.Pow(10, nomer);
+
+
+
+                  
+            
+
+        }
+    }
+}
