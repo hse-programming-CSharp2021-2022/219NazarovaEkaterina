00462b9c4d2df125ad28701c7cc5899a1e27042f
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:43:14 +0300

    Add files via upload

---
+++ module_01/Seminar_06/Classwork/Task_02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[6];
+            int prom;
+            Random r = new Random();
+            for(int i = 0; i < 6; i++)
+            {
+                int.TryParse(Console.ReadLine(), out mas[i]);
+            }
+            for(int i = 0; i < 6; i++)
+            {
+                Console.Write("{0} ", mas[i]);
+            }
+            Console.WriteLine();
+            int k = mas.Length;
+            for(int i = 0; i < k - 1; i++)
+            {
+                prom = mas[i] + mas[i + 1];
+                if (prom % 3 == 0)
+                {
+                    mas[i] = mas[i] * mas[i + 1];
+                    for(int j = i+2; j <= mas.Length-1; j++)
+                    {
+                        mas[j - 1] = mas[j];
+                    }
+                    k--;
+                }
+            }
+            for(int i = 0; i < k; i++)
+            {
+                Console.Write("{0} ", mas[i]);
+            }
+        }
+    }
+}
