191d56191f602c34c1f4a4ea932e67c31ba4badc
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-17 00:25:48 +0300

    Add files via upload

---
+++ module_01/Seminar_03/Classwork/Task_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int i;
+            int.TryParse(s, out i);
+            switch (i)
+            {
+                case <=3:
+                    Console.WriteLine("неудовлетворительно");
+                    break;
+                case <= 5:
+                    Console.WriteLine("удовлетворительно");
+                    break;
+                case <=7:
+                    Console.WriteLine("удовлетворительно");
+                    break;
+                case <= 10:
+                    Console.WriteLine("отлично");
+                    break;
+                default:
+                    Console.WriteLine("нет такой оценки");
+                    break;
+            }
+        }
+    }
+}
