6e1bf1fa25eb0ce9ae873f528111350279542d83
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-10-16 23:37:11 +0300

    Add files via upload

---
+++ module_01/Seminar_01/Classwork/Task_05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int aa, bb;
+            int.TryParse(a, out aa);
+            int.TryParse(b, out bb);
+            Console.WriteLine(aa + bb);
+            Console.WriteLine(aa - bb);
+            Console.WriteLine(aa * bb);
+            Console.WriteLine(aa / bb);
+            Console.WriteLine(aa % bb);
+            Console.WriteLine(aa >> bb);
+            Console.WriteLine(aa << bb);
+        }
+    }
+}
