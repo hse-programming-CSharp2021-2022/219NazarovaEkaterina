9a22b15c936e9f5fd0b0f7debf707d0f763f247c
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-11-05 02:41:36 +0300

    Add files via upload

---
+++ module_02/Seminar_02/Classwork/Task_01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task_01
+{
+
+    class MyComplex
+    {
+        public double re, im;
+        public MyComplex(double xre, double xim)
+        { re = xre; im = xim; }
+        public static MyComplex operator ++(MyComplex mc)
+        { return new MyComplex(mc.re + 1, mc.im + 1); }
+        public static MyComplex operator --(MyComplex mc)
+        { return new MyComplex(mc.re - 1, mc.im - 1); }
+        public double Mod() { return Math.Abs(re * re + im * im); }
+        static public bool operator true(MyComplex f)
+        {
+            if (f.Mod() > 1.0) return true;
+            return false;
+        }
+        static public bool operator false(MyComplex f)
+        {
+            if (f.Mod() <= 1.0) return true;
+            return false;
+        }
+        public static MyComplex operator + (MyComplex first, MyComplex second)
+        {
+            return new MyComplex(first.re + second.re, first.im + second.im);
+        }
+        public static MyComplex operator - (MyComplex first, MyComplex second)
+        {
+            return new MyComplex(first.re - second.re, first.im - second.im);
+        }
+        public static MyComplex operator * (MyComplex first, MyComplex second)
+        {
+            return new MyComplex(first.re * second.re - first.im * second.im, first.im * second.re + first.re * second.im);
+        }
+        public static MyComplex operator / (MyComplex first, MyComplex second)
+        {
+            return new MyComplex((double)(first.re * second.re + first.im * second.im) / (double)(second.re * second.re + second.im * second.im),
+                (double)(first.im * second.re - first.re * second.im) / (double)(second.re * second.re + second.im * second.im));
+        }
+        public override string ToString()
+        {
+            return $"{this.re} + ({this.im})i";
+        }
+    }
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new MyComplex(1, 2);
+            var b = new MyComplex(2, 3);
+            Console.WriteLine(a.ToString());
+            Console.WriteLine((b).ToString());
+            Console.WriteLine((a + b).ToString());
+            Console.WriteLine((b - a).ToString());
+            Console.WriteLine((a * b).ToString());
+            Console.WriteLine((b / a).ToString());
+        }
+    }
+}
