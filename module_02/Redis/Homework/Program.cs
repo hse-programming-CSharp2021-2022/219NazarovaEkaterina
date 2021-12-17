fb4dbfc556235034f91a50f9eb9ee8d73cf8abba
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-12-17 23:58:16 +0300

    Add files via upload

---
+++ module_02/Redis/Homework/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TaskListSolution
+{
+    // Сиквел.
+    // Разработчики из HSE company просят доработать ваше приложение!
+    // Дело в том, что разработчики тоже ошибаются, и приходится откатывать приложение к предыдущей версии.
+    // К тому же, HSE company не хочет расходовать много памяти,
+    // поэтому было принято решение хранить только определенное колличество последних версий приложений.
+    // 
+    // На вход программе подаются запросы следующего типа:
+    // 1) add <application_name> <version> - добавить актуальную версию приложения.
+    // 2) back <application_name> - откатить приложение до предыдущей версии. Если предыдущей нет, то удалить приложение.
+    // 3) get <application_name> - получить актуальную версию приложения. Если приложения нет, то сообщить об этом.
+    // 4) exit - завершить программу.
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RedisClient.Connect();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Input: ");
+                string command = Console.ReadLine();
+
+                string[] words = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name;
+                switch (words[0])
+                {
+                    case ("add"):
+                        name = words[1];
+                        string version = words[2];
+                        RedisClient.Add(name, version);
+                        //Console.WriteLine("Version added");
+                        break;
+                    case ("back"):
+                        name = words[1];
+                        string result = RedisClient.Back(name);
+                        Console.WriteLine(result);
+                        break;
+                    case ("get"):
+                        name = words[1];
+                        string resultTwo = RedisClient.Get(name);
+                        Console.WriteLine(resultTwo);
+                        break;
+                    case ("exit"):
+                        return;
+                    default:
+                        Console.WriteLine("Incorrect query");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ module_02/Redis/Homework/RedisClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using StackExchange.Redis;
+
+namespace TaskListSolution
+{
+    public static class RedisClient
+    {
+        /// <summary>
+        /// Maximum number of versions to store
+        /// </summary>
+        public const uint MaxCount = 5;
+
+        private static ConnectionMultiplexer redis;
+        private static IDatabase database;
+
+        public static void Connect(string connectionString = "localhost")
+        {
+            //string connection = "redis-13906.c100.us-east-1-4.ec2.cloud.redislabs.com:13906";
+            redis = ConnectionMultiplexer.Connect("redis-13906.c100.us-east-1-4.ec2.cloud.redislabs.com:13906, password=OkHUU3wYW2sXlMlvsDGq3z9wCQ8U5Ahx, ConnectTimeout=10000, allowAdmin=true");
+            database = redis.GetDatabase();
+            //IServer se = redis.GetServer(connection, 13906);
+            database = redis.GetDatabase();
+            Console.WriteLine("Connected!");
+        }
+
+        public static string Get(string key)
+        {
+            if (!Exist(key))
+            {
+                Console.WriteLine("Application not found");
+                return "";
+            }
+            else
+            {
+                return database.ListGetByIndex(key, database.ListLength(key) - 1);
+            }
+        }
+
+        public static bool Exist(string key)
+        {
+            return database.KeyExists(key);
+        }
+
+        public static void Add(string key, string value)
+        {
+            if (Exist(key))
+            {
+                if (database.ListLength(key) <= MaxCount)
+                    database.ListRightPush(key, value);
+                else
+                    Console.WriteLine("Too much versions");
+            }
+            else
+            {
+                database.ListRightPush(key, value);
+            }
+        }
+
+        public static string Back(string key)
+        {
+            if (database.ListLength(key) == 0)
+            {
+                Console.WriteLine("No active versions. This application was deleated");
+                database.KeyDelete(key);
+                return "";
+            }
+            else
+            {
+                long len = database.ListLength(key);
+                database.ListRemove(key, database.ListGetByIndex(key, len - 1));
+                return database.ListGetByIndex(key, len - 2);
+            }
+        }
+    }
+}
