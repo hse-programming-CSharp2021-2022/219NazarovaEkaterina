3ffe8868e91ae5f82d02730dd8b729aa9dcaab8b
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-12-17 23:59:16 +0300

    Add files via upload

---
+++ module_02/Redis/Homework/Task_04/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskSetSolution
+{
+    // Сиквел.
+    // У Склад.LIFE большое количество различных складов с различными видами товаров.
+    // Руководству важно знать, какие виды товаров находятся на различных складах. Помогите Склад.LIFE. 
+    // P.S. В последнее время с заказами все плохо, поэтому на склад только завозят новые виды товаров.
+    //
+    // На вход программе поступают следующие запросы:
+    // 1) add <storage_name> <product_name> - добавить вид товара на склад.
+    // 2) get <storage_name> - получить список всех видов товаров на складе.
+    // 3) exist <storage_name> <product_name> - узнать находится ли вид товара на складе.
+    // 4) exit - завершить программу.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RedisClient.Connect();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Input: ");
+                string command = Console.ReadLine();
+
+                string[] words = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name;
+                switch (words[0])
+                {
+                    case ("add"):
+                        name = words[1];
+                        string version = words[2];
+                        RedisClient.Add(name, version);
+                        //Console.WriteLine("Version added");
+                        break;
+                    case ("exist"):
+                        name = words[1];
+                        string product = words[2];
+                        bool result = RedisClient.ExistProduct(name, product);
+                        Console.WriteLine(result);
+                        break;
+                    case ("get"):
+                        name = words[1];
+                        List<string> resultTwo = RedisClient.GetProducts(name);
+                        foreach (string s in resultTwo)
+                            Console.WriteLine(s);
+                        break;
+                    case ("exit"):
+                        return;
+                    default:
+                        Console.WriteLine("Incorrect query");
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
---
+++ module_02/Redis/Homework/Task_04/RedisClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StackExchange.Redis;
+
+namespace TaskSetSolution
+{
+    public static class RedisClient
+    {
+        private static ConnectionMultiplexer redis;
+        private static IDatabase database;
+
+        public static void Connect(string connectionString = "localhost")
+        {
+            redis = ConnectionMultiplexer.Connect("redis-13906.c100.us-east-1-4.ec2.cloud.redislabs.com:13906, password=OkHUU3wYW2sXlMlvsDGq3z9wCQ8U5Ahx, ConnectTimeout=10000, allowAdmin=true");
+            database = redis.GetDatabase();
+            database = redis.GetDatabase();
+            Console.WriteLine("Connected!");
+        }
+
+        public static void Add(string key, string value)
+        {
+            //if (Exist(key))
+            //{
+                database.SetAdd(key, value);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Storage does not exist");
+            //}
+        }
+
+        public static bool Exist(string key)
+        {
+            return database.KeyExists(key);
+        }
+
+        public static bool ExistProduct(string key, string productName)
+        {
+            return database.SetContains(key, productName);
+        }
+
+        public static List<string> GetProducts(string key)
+        {
+            RedisValue[] values = database.SetMembers(key);
+            return values.ToStringArray()
+                .ToList();
+        }
+    }
+}
