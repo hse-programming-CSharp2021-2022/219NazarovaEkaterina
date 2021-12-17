0abb36ebddbb0cd49bb862d468d5bac227aabc4a
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-12-18 00:04:24 +0300

    Add files via upload

---
+++ module_02/Seminar_2311/Homework/Чеширский_кот/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Homework_from2311
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerO = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timerO
+            // 
+            this.timerO.Interval = 1000;
+            this.timerO.Tick += new System.EventHandler(this.timerO_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(71, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(691, 153);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Чеширский кот";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Растаявшая надпись";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerO;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ module_02/Seminar_2311/Homework/Чеширский_кот/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_from2311
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool Back = false; 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Чеширский кот";
+            timerO.Enabled = true;
+            Back = false;
+        }
+
+        private void timerO_Tick(object sender, EventArgs e)
+        {
+            if (!Back)
+            {
+                this.Opacity -= 0.1;
+                if(this.Opacity == 0)
+                {
+                    Back = true;
+                    if (label1.Text == "Чеширский кот")
+                        label1.Text = "Кот уже ушёл";
+                    else
+                        label1.Text = "Чеширский кот";
+                }
+            }
+            else
+            {
+                this.Opacity += 0.1;
+                if(this.Opacity == 1)
+                {
+                    Back = false;
+                }
+            }
+        }
+    }
+}
