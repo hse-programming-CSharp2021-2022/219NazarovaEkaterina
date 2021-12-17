d01184ebec78778aa965e321c980c249917dcde5
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-12-18 00:07:02 +0300

    Add files via upload

---
+++ module_02/Seminar_2611/Class&Homework/Task_01/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Task_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.textBoxNext = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(13, 34);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(268, 57);
+            this.buttonNext.TabIndex = 0;
+            this.buttonNext.Text = "Следующий член ряда!";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // textBoxNext
+            // 
+            this.textBoxNext.Location = new System.Drawing.Point(13, 119);
+            this.textBoxNext.Multiline = true;
+            this.textBoxNext.Name = "textBoxNext";
+            this.textBoxNext.Size = new System.Drawing.Size(389, 38);
+            this.textBoxNext.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxNext);
+            this.Controls.Add(this.buttonNext);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.TextBox textBoxNext;
+    }
+}
+
---
+++ module_02/Seminar_2611/Class&Homework/Task_01/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_01
+{
+    public partial class Form1 : Form
+    {
+        public int p1 = 1, p2 = 2;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int prom = p1;
+                p1 = p2;
+                p2 = checked(prom + 2 * p1);
+                textBoxNext.Text = p2.ToString();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Переполнение! Ряд начинается сначала!");
+                p1 = 1;
+                p2 = 2;
+            }
+        }
+    }
+}
