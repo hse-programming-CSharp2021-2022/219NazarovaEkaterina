44d2e855226e32b5fa0f60d3b5fd0449e8c04727
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-12-18 00:07:36 +0300

    Add files via upload

---
+++ module_02/Seminar_2611/Class&Homework/Task_02/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Ефыл_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonReal = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonChanged = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonReal
+            // 
+            this.buttonReal.Location = new System.Drawing.Point(13, 24);
+            this.buttonReal.Name = "buttonReal";
+            this.buttonReal.Size = new System.Drawing.Size(275, 29);
+            this.buttonReal.TabIndex = 0;
+            this.buttonReal.Text = "Показать исходный список";
+            this.buttonReal.UseVisualStyleBackColor = true;
+            this.buttonReal.Click += new System.EventHandler(this.buttonReal_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(13, 71);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(275, 175);
+            this.textBoxResult.TabIndex = 1;
+            // 
+            // buttonChanged
+            // 
+            this.buttonChanged.Location = new System.Drawing.Point(13, 262);
+            this.buttonChanged.Name = "buttonChanged";
+            this.buttonChanged.Size = new System.Drawing.Size(275, 29);
+            this.buttonChanged.TabIndex = 2;
+            this.buttonChanged.Text = "Показать изменения";
+            this.buttonChanged.UseVisualStyleBackColor = true;
+            this.buttonChanged.Click += new System.EventHandler(this.buttonChanged_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonChanged);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.buttonReal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonReal;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button buttonChanged;
+    }
+}
+
---
+++ module_02/Seminar_2611/Class&Homework/Task_02/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ефыл_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach(string x in textBoxResult.Lines)
+            {
+                array.Add(x);
+                array2.Add(x);
+            }
+        }
+
+        public List<string> array = new List<string>();
+        public List<string> array2 = new List<string>();
+
+        private void buttonReal_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Lines = array2.ToArray();
+        }
+
+        private void buttonChanged_Click(object sender, EventArgs e)
+        {
+            string p = textBoxResult.Lines.ToArray().Join(" ");
+            string[] a = { "aaaaa", "bbbbb" };
+            string
+        }
+    }
+}
