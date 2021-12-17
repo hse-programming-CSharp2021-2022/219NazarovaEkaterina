3e1cea3e93d99db3b7631fb04ca8e9b5968c793d
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2021-12-18 00:05:08 +0300

    Add files via upload

---
+++ module_02/Seminar_2311/Homework/ListBox/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach(string s in mas)
+            {
+                listBox1.Items.Add(s);
+            }
+        }
+
+        List<string> mas = new List<string>{ "aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee" };
+
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            if(listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Список пуст!");
+                return;
+            }
+            try
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Список пуст или нет выбранного элемента");
+            }
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (string s in mas)
+            {
+                listBox1.Items.Add(s);
+            }
+        }
+    }
+}
