5dd9e3225f2ef5c4462823a2bc6ea72c70c3cc52
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2022-01-18 14:04:26 +0300

    Add files via upload

---
+++ module_03/Seminar_01/Homework/Task_01(1)/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Task_01_1_
+{
+    class Program
+    {
+        public delegate int Cast(double t);
+        
+        static int Example2(double t)
+        {
+            int prom = (int)Math.Log10(t);
+            return prom + 1;
+        }
+
+        static int Example1(double t)
+        {
+            int p = (int)t;
+            int p1 = (p % 2 == 0) ? p : (p - 1);
+            int p2 = ((p + 1) % 2 == 0) ? (p + 1) : (p + 2);
+            if (Math.Abs(t - p1) > Math.Abs(t - p2))
+                return p2;
+            else
+                return p1;
+        }
+        
+        static void Main(string[] args)
+        {
+            Cast first = delegate (double t)
+            {
+                int p = (int)t;
+                int p1 = (p % 2 == 0) ? p : (p - 1);
+                int p2 = ((p + 1) % 2 == 0) ? (p + 1) : (p + 2);
+                if (Math.Abs(t - p1) > Math.Abs(t - p2))
+                    return p2;
+                else
+                    return p1;
+            };
+
+            Cast second = delegate (double t)
+            {
+                int p = (int)t;
+                int counter = 0;
+                while (p > 0)
+                {
+                    p /= 10;
+                    counter++;
+                }
+                return counter;
+            };
+
+            Console.WriteLine(first(20.15));
+            Console.WriteLine(second(20.15));
+            Console.WriteLine(first(2.35));
+            Console.WriteLine(second(2.35));
+
+            first += second;
+            Console.WriteLine(first(20.5));
+
+            first = (double t) =>
+            {
+                int p = (int)t;
+                int p1 = (p % 2 == 0) ? p : (p - 1);
+                int p2 = ((p + 1) % 2 == 0) ? (p + 1) : (p + 2);
+                if (Math.Abs(t - p1) > Math.Abs(t - p2))
+                    return p2;
+                else
+                    return p1;
+            };
+            second = (double t) =>
+            {
+                int p = (int)t;
+                int counter = 0;
+                while (p > 0)
+                {
+                    p /= 10;
+                    counter++;
+                }
+                return counter;
+            };
+
+            first += second;
+            Console.WriteLine(first(20.5));
+
+            Console.WriteLine(first.Invoke(20.5));
+
+            first = Program.Example1;
+            first += Program.Example2;
+            first.Invoke(20.5);
+            first -= Program.Example2;
+            first.Invoke(20.5);
+
+
+
+        }
+    }
+}
