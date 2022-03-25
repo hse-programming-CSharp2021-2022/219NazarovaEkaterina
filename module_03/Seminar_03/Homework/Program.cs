6c2595994f7a6a640e5781861e82d7cbff5d7b7c
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2022-03-25 22:50:49 +0300

    Add files via upload

---
+++ module_03/Seminar_03/Homework/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Homework_3
+{
+    delegate void CoordChanged(Dot point);
+    
+    class Dot
+    {
+        public Dot(double x, double y)
+        {
+            (X, Y) = (x, y);
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public event CoordChanged OnCoordChanged;
+
+        public void DotFlow()
+        {
+            Random ran = new();
+            for(int i = 0; i < 10; i++)
+            {
+                X = ran.Next(-10, 10) + ran.NextDouble();
+                Y = ran.Next(-10, 10) + ran.NextDouble();
+                if(X < 0 && Y < 0)
+                {
+                    OnCoordChanged.Invoke(this);
+                }
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void PrintInfo(Dot A) => Console.WriteLine($"X: {A.X:F4}, Y: {A.Y:F4}");
+        
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            Dot d = new(x, y);
+            d.OnCoordChanged += PrintInfo;
+            d.DotFlow();
+        }
+
+        
+    }
+}
