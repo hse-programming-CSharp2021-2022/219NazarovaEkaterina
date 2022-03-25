f916d53adc461032d761be9996d04c43194a0a25
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2022-03-25 22:53:06 +0300

    Add files via upload

---
+++ module_03/Seminar_05/Homework/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Homework_5
+{
+    public interface IFunction
+    {
+        public double Function(double x);
+    }
+
+    public delegate double Calculate(double x);
+
+    class F : IFunction
+    {
+
+        public F(Calculate c) => calculate = c; 
+        public Calculate calculate { get; set; }
+
+        public double Function(double x)
+        {
+            return calculate.Invoke(x);
+        }
+    }
+
+    class G
+    {
+        public G(F f, F g) => (_fx, _gx) = (f, g);
+        
+        F _fx, _gx;
+
+        public double GF(double x0) => _gx.Function(_fx.Function(x0));
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            F f1 = new((x) => x * x - 4);
+            F f2 = new((x) => Math.Sin(x));
+
+            G g = new(f2, f1);
+
+            for(int i = 0; i <= 16; i++)
+            {
+                Console.WriteLine($"{i} * pi / 16:\t{g.GF(i * Math.PI / 16):F4}");
+            }
+        }
+    }
+}
