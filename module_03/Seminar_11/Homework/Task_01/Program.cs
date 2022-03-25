cdd577ff6111c456728791d17b26a806d3dec9a5
Katrina-17 <90283523+Katrina-17@users.noreply.github.com>
2022-03-25 22:59:13 +0300

    Add files via upload

---
+++ module_03/Seminar_11/Homework/Task_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Homework_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //В первом проекте, создать бинарный файл Numbers и записать в него средствами класса
+            //BinaryWriter 10 целых чисел, случайно выбранных из интервала [1;100].
+
+            Random ran = new();
+            using (BinaryWriter bw = new(new FileStream("Numbers.bin", FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    bw.Write(ran.Next(1, 101));
+                }
+            }
+        }
+    }
+}
